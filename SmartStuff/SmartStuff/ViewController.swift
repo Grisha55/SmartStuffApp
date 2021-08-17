b730bb8da7276a5d458a6858a95590e8e25b712b
Grigory <grishavinyar@icloud.com>
2021-08-17 17:28:42 +0300

    Init the project

---
+++ SmartStuff/SmartStuff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartStuff
+//
+//  Created by Григорий Виняр on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
